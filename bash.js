46c707dffa8978003032d8e046b2ce73c6758129
almondmilk96 <kara.hirschman@gmail.com>
2018-06-12 12:11:41 -0400

    fixed typo

--- bash.js
+++ bash.js
@@ -2,5 +2,5 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
     const cmd = data.toString().trim();
     process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('prompt > ');
+    process.stdout.write('\nprompt > ');
 })
